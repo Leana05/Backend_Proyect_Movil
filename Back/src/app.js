e6fc8389604416b7eb65adb1e16842e8e904a77e
Leana05 <yelyl471@gmail.com>
2024-05-31 21:33:52 -0500

    Ruta y controller de productos listo

--- Back/src/app.js
+++ Back/src/app.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import registerUser from './routes/registerUser.routes.js';
-import indexRoutes from './routes/index.routes.js';
 import mascotaRoutes from './routes/mascota.routes.js';
+import productoRoutes from './routes/product.routes.js';
 
 const app = express();
 
 app.use(express.json()); //Nos ayuda a tranformar las solicitudes a Json
 
 app.use(registerUser);
-app.use(indexRoutes);
 app.use(mascotaRoutes);
+app.use(productoRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({
